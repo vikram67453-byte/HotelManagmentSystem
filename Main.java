3ca3520b8cef3313a1b2eeef99f85b324c134a62
vikram67453-byte <vikram.67453@iqra.edu.pk>
2026-01-19 17:00:56 +0530

    changes in main class

--- Main.java
+++ Main.java
@@ -1,31 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Create Hotel
         Hotel hotel = new Hotel("Grand Palace");
 
-        // Add Rooms
         hotel.addRoom(new Room(101, 5000));
         hotel.addRoom(new Room(102, 6000));
 
-        // Create Customer
         Customer customer = new Customer("Ali Khan", "35202-1234567-1");
 
-        // Book Room
         Room room = hotel.findAvailableRoom();
         Booking booking = new Booking(room, customer, 3);
 
-        // Billing
         double bill = booking.calculateBill();
         Payment payment = new Payment(bill);
 
-        // Output
         System.out.println("Hotel Name: " + hotel.getName());
         System.out.println("Customer Name: " + customer.getName());
         System.out.println("Room Number: " + room.getRoomNumber());
         System.out.println("Total Bill: " + payment.getAmount());
 
-        // Checkout
         booking.checkout();
         System.out.println("Checkout successful");
     }
